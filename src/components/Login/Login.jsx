44ea81e85b72db74c45fe68dda5e958433086201
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-01 16:07:44 -0600

    Se agrega nuevo componente Login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { Input } from '../Input'
+import { loginUser } from '../../services/api'
+import { Link } from 'react-router-dom'
+import { useLogin } from '../../shared/hooks/useLogin'
+
+export const Login = () => {
+    const [userInformation, setUserInformation]= useState("")
+    const [password, setPassword]= useState("")
+
+    const [formValidation, setFormValidation] = useState({
+        userInformation: undefined,
+        password: undefined
+    })
+
+    const disabled = formValidation.userInformation === "" && formValidation.password === ""
+    
+    const {login, setError, loading, error} =useLogin()
+
+    const handleSubmit = (event)=>{
+        event.preventDefault()
+        login(userInformation, password)
+    }
+
+    const handleChangeUserInformation = (event)=>{
+        const value = event.target.value
+        setFormValidation({...formValidation, userInformation: value.length>0?'':'Please field this field'})
+        setUserInformation(value)
+    }
+
+    const handleChangePassword = (event)=>{
+        const value = event.target.value
+        setFormValidation({...formValidation, password: value.length>0?'':'Please field this field'})
+        setPassword(value)
+    }
+
+    return (
+        <>
+        <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+            <p>Don't have an account? <Link to="/register">Register</Link></p>
+                <Input field='dataInformation' label='Username or Email' type='text' value={userInformation} handleValueChange={handleChangeUserInformation}/>
+
+                <Input field='password' label='Password' type='password' value={password} handleValueChange={handleChangePassword}/>
+
+                <button>Sign in</button>
+            </form>
+        </>
+    )
+}
