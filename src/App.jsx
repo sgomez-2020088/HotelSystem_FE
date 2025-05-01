f8d487301105508312710d3601d2afa351348c38
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-01 12:04:15 -0600

    Se modific  a el archivo app, agregandole el Toaster

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, useRoutes } from "react-router-dom"
 import { routes } from "./routes"
+import { Toaster } from "react-hot-toast"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       {elements}
+      <Toaster position='bottom-right' reverserOrder={false}/>
     </>
   )
 }
