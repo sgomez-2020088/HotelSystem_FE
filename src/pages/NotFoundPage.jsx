56243e331a09fa268a2a86fb4c956cc897c978a5
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-04-30 18:31:49 -0600

    Se agrega nueva carpeta Pages

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const NotFoundPage = () => {
+    return (
+        <>
+            <h1>Error 404</h1>
+            <h1>Page not Found</h1>
+        </>
+    )
+}
---
+++ src/pages/Register/RegisterPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Register } from '../../components/Register/Register.jsx'
+import './RegisterPage.css'
+
+export const RegisterPage = () => {
+    return (
+        <Register/>
+    )
+}
