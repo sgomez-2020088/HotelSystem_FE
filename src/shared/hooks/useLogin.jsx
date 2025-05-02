e0c4d57421ee7952e7267c8ca7fa0d4f1746ebeb
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-01 19:21:51 -0600

    Se agreg modifica hook useLogin

--- src/shared/hooks/useLogin.jsx
+++ src/shared/hooks/useLogin.jsx
@@ -1,23 +1,29 @@
 import React, { useState } from 'react'
 import { loginUser } from '../../services/api'
 import toast from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
 
 export const useLogin = () => {
+        
         const [loading, setLoading] = useState(false)
         const [error, setError] = useState(false)
-    
+        const navigate = useNavigate();
+
         const login = async(userInformation, password) => {
             setLoading(true)
+            
             const user = {
                 userInformation,
                 password
             }
             const response = await loginUser(user)
             setLoading(false)
+            
             if (response.error) {
                 setError(true)
                 
                 if(response?.err?.response?.data?.errors){
+                    
                     let arrayErrors = response?.err?.response?.data?.errors
                     for (const error of arrayErrors) {
                         return toast.error(error.msg)
@@ -30,8 +36,10 @@ export const useLogin = () => {
                     'Error general al intentar registrar al usuario. Intenta de nuevo'
                 )
             }
-            setError(false)
-            return toast.success('gogogo')
+            
+            setError(false)         
+            navigate('/')
+            return toast.success('Login exitoso')
         }
     return {
         login,
