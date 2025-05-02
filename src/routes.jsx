54a5263d2d26d0a36d228111e496799c5d5a74e8
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-01 19:21:20 -0600

    Se modifica archivos de rutas

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import { NotFoundPage } from './pages/NotFoundPage.jsx'
 import { RegisterPage } from './pages/Register/RegisterPage.jsx'
 import { WelcomePage } from './pages/WelcomePage.jsx'
 
+
 export const routes = [
     {path:'/',element:<WelcomePage/>},
     {path:'/register', element:<RegisterPage/>},
