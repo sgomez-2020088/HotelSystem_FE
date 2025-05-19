3d970affe2f77db8859e037f996249abcad47c87
jsican-2023091 <jsican-2023091@kinal.edu.gt>
2025-05-19 11:41:43 -0600

    Se agrega el formulario para NewHotel

--- src/components/NewHotel/NewHotel.jsx
+++ src/components/NewHotel/NewHotel.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAddHotel } from '../../shared/hooks/useAddHotel'
+import { Input } from '../Input'
+import './NewHotelCSS.css'
 
 export const NewHotel = () => {
+  const [name, setName] = useState('')
+  const [address, setAddress] = useState('')
+  const [category, setCategory] = useState('')
+  const [amenities, setAmenities] = useState('')
+
+  const { addHotel } = useAddHotel()
+
+  const handleSubmit = (event) =>{
+    event.preventDefault()
+    addHotel(name, address, category, amenities)
+  }
+
+  const handleChangeName = (event) => {
+    const value = event.target.value
+
+    setName(value)
+  }
+
+  const handleChangeAdrees = (event) => {
+    const value = event.target.value
+
+    setAddress(value)
+  }
+
+  const handleChangeCategory = (event) => {
+    const value = event.target.value
+
+    setCategory(value)
+  }
+
+  const handleChangeAmenities = (event) => {
+    const value = event.target.value
+
+    setAmenities(value)
+  }
   return (
-    <div>NewHotel</div>
+    <div className="container">
+      <h1>NewHotel</h1>
+        <form onSubmit={handleSubmit}>
+          <p>Guarda tu nuevo hotel</p>
+            <Input field='name' label='Name' type="text" value={name} handleValueChange={handleChangeName}/>
+.
+            <Input field='address' label='Address'type="text" value={address} handleValueChange={handleChangeAdrees}/>
+
+            <Input field='category' label='Category' type="text" value={category} handleValueChange={handleChangeCategory}/>
+
+            <Input field='amenities' label='Amenities' type="text" value={amenities} handleValueChange={handleChangeAmenities}/>
+
+            <button >Guardar</button>
+        </form>
+    
+    </div>
   )
 }
