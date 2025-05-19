8955f44f779f705321075e7cf53d8c8ccb248dcc
ndelacruz-2023048 <ndelacruz-2023048@kinal.edu.gt>
2025-05-19 10:20:50 -0600

    Se agrega css para sidebar y navbar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export const SideBar = ({children,classname}) => {
+export const SideBar = ({children}) => {
   return (
-    <div className={classname}>{children}</div>
+    <div style={{ backgroundColor: '#333333', width: '180px', padding: '5px', color:'#ffffff' }}>{children}</div>
   )
 }
--- src/pages/Hotels/HotelsPage.jsx
+++ src/pages/Hotels/HotelsPage.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
-
 import { SideBar } from '../../components/SideBar/SideBar'
 import { NavBar } from '../../components/Navbar/NavBar'
 import { HotelContent } from '../../components/HotelContent'
+import { Link } from 'react-router-dom'
+
+import './HotelPageCss.css'
 
 export const HotelsPage = () => {
   return (
     <>
-        <NavBar>
-            
-        </NavBar>
-        <SideBar>
+      <NavBar className="navbar">
+        <h1>Hoteles</h1>
+      </NavBar>
 
+      <div className="main-layout">
+        <SideBar className="sidebar">
+          <ul>
+            <li><Link to="/hoteles/agregar" className="sidebar-link">Agregar</Link></li>
+            <li><Link to="/hoteles/editar" className="sidebar-link">Editar</Link></li>
+            <li><Link to="/hoteles/eliminar" className="sidebar-link">Eliminar</Link></li>
+          </ul>
         </SideBar>
 
-        <HotelContent/>
-
+        <HotelContent className="content"/>
+      </div>
     </>
   )
 }
