d2853aad5e936f9d0b9e94d80cceeb1654a4931d
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-19 10:50:02 -0600

    Se crean el hook useHotels

---
+++ src/shared/hooks/useHotels.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { getHotelsRequest } from '../../services/api'
+
+export const useHotels = () => {
+    const [hotels, setHotels] = useState(null)
+    
+    const getHotels = async()=>{
+        const response = await getHotelsRequest()
+        if(response.error)
+            return toast.error(
+                response?.err?.response?.data?.message || 
+                'Error al obtener los hoteles, por favor intente mas tarde'
+            )
+    }
+    setHotels(response.data.hotels)
+    return{
+        hotels,
+        isFetching: !hotels,
+        getHotels
+  }
+}
