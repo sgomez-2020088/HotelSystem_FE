df7cee630ef0aff062465d48c79c33eab6e3f39d
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-19 23:08:20 -0600

    Se agrega renderizado de comentarios

--- src/components/Hotel/Hotels.jsx
+++ src/components/Hotel/Hotels.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useHotels } from '../../shared/hooks/useHotels'
+import {CardHotel} from '../Hotel/CardHotel'
+import { useOutletContext } from 'react-router-dom'
+import { ScaleLoader } from 'react-spinners'
 
 export const Hotels = () => {
-  
+
+const {hotels,getHotels,isFetching}=useHotels()
+const [render, setRender] = useState(true)
+
+useEffect(() => {
+    getHotels()
+  }, [])
+  console.log(render)
+  if(isFetching){
+    return(
+      <div>
+        <ScaleLoader height={80} />
+      </div>
+    )
+  }
+
   return (
-    <div>Hotels</div>
+    <div>
+      {
+        hotels.map((hotel)=>(<CardHotel key={hotel._id} name={hotel.name} address={hotel.address} category={hotel.category} amenities={hotel.amenities}/>))
+      }
+      
+    </div>
   )
 }
