25b161f11a6a04a99ac8a4cc5739cf63d9ec9e89
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-20 23:11:13 -0600

    Se agrega nueva ruta

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,7 +5,8 @@ import { LoginPage } from './pages/Login/LoginPage.jsx'
 import { NotFoundPage } from './pages/NotFoundPage.jsx'
 import { RegisterPage } from './pages/Register/RegisterPage.jsx'
 import { WelcomePage } from './pages/WelcomePage.jsx'
-
+import { UpdateHotel } from './components/updateHotel/updateHotel.jsx'
+import { DeleteHotel } from './components/DeleteHotel/DeleteHoteL.jsx'
 
 export const routes = [
     {path:'/',element:<WelcomePage/>},
@@ -16,7 +17,9 @@ export const routes = [
         element:<HotelsPage/>,
         children:[
             {path:'addHotel', element:<NewHotel/>},
-            {path:'Hotels', element: <Hotels/>}
+            {path:'Hotels', element: <Hotels/>},
+            {path:'updateHotel/:id', element: <UpdateHotel/>},
+            {path:'deleteHotel/:id', element:<DeleteHotel/>}
         ]
     },
     {path:'*', element:<NotFoundPage/>}
