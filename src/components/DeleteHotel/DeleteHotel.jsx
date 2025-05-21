bd35a339fa0cb450f53496a6d30aeae058809087
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-20 23:12:40 -0600

    Se crea componente deleteHotel

---
+++ src/components/DeleteHotel/DeleteHotel.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import { useHotelById } from '../../shared/hooks/useHotelById'
+import { useParams } from 'react-router-dom'
+import { useDeleteHotel } from '../../shared/hooks/useDeleteHotel'
+import { ScaleLoader } from 'react-spinners'
+import { Input } from '../Input'
+
+import '../NewHotel/NewHotelCSS.css'
+import toast from 'react-hot-toast'
+export const DeleteHotel = () => {
+  const {hotel,isFetching, getHotelById} = useHotelById() 
+   
+  const [name, setName] = useState('') 
+  
+  const {id}= useParams()
+
+   const {deleteHotel} = useDeleteHotel()
+
+   useEffect(()=>{
+           getHotelById(id)
+       },[])
+
+  if(isFetching){
+    return(
+      <div>
+        <ScaleLoader height={80} />
+      </div>
+    )
+  }
+
+  const handleChangeName = (e) =>{
+    const value = e.target.value
+    setName(value)
+  }
+
+  const handleDeleteHotel = () =>{
+    if(name === hotel.name){
+        deleteHotel(id)
+    }else{
+        toast.error('No son los mismos titulos')
+    }
+  }
+
+  console.log(hotel)
+  return (
+    <div className='form-wrapper'>
+      <Input field='name'label='Rescriba el nombre del hotel para confirmar su eliminación' type='text' value={name} handleValueChange={handleChangeName}/>
+      <button onClick={handleDeleteHotel}>Eliminar</button>
+    </div>
+  )
+}
