03b6dd3604875b1128a75a1a36ff483f2b1e37a7
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-20 20:32:45 -0600

    Se agrega elemento select con opciones

--- src/components/NewHotel/NewHotel.jsx
+++ src/components/NewHotel/NewHotel.jsx
@@ -8,6 +8,7 @@ export const NewHotel = () => {
   const [address, setAddress] = useState('')
   const [category, setCategory] = useState('')
   const [amenities, setAmenities] = useState('')
+  //const [hotelImage, setHotelImage] = useState(null)
 
   const { addHotel } = useAddHotel()
 
@@ -30,24 +31,48 @@ export const NewHotel = () => {
 
   const handleChangeCategory = (event) => {
     const value = event.target.value
-
+    console.log(value)
     setCategory(value)
   }
 
   const handleChangeAmenities = (event) => {
     const value = event.target.value
-
+    
     setAmenities(value)
   }
+
+   /*const handleChangeImageHotel = (event) => {
+    const file = event.target.files[0]
+    console.log(file)
+    setHotelImage(file)
+  }*/
+  
   return (
   <div className="form-wrapper">
     <form className='form-container' onSubmit={handleSubmit}>
       <h1>Nuevo hotel</h1>
       <p>Guarda tu nuevo hotel</p>
       <Input field='name' label='Name' type="text" value={name} handleValueChange={handleChangeName}/>
-      <Input field='address' label='Address'type="text" value={address} handleValueChange={handleChangeAdrees}/>
-      <Input field='category' label='Category' type="text" value={category} handleValueChange={handleChangeCategory}/>
+      <Input field='address ' label='Address'type="text" value={address} handleValueChange={handleChangeAdrees}/>
+      <label>Categoroy</label>
+      <select onChange={handleChangeCategory} value={category}>
+        <option value='Category'>Category</option>
+        <option value='Hotel'>Hotel</option>
+        <option value='Hostal'>Hostal</option>
+        <option value='Casa de huéspedes'>Casa de huéspedes</option>
+        <option value='HotelNegocios'>Hotel de Negocios</option>
+        <option value='HotelBoutique'>Hotel de Boutique</option>
+        <option value='ComplejoHotelero'>Complejo Hotelero</option>
+        <option value='HotelAeropuerto'>Hotelde Aeropuerto</option>
+        <option value='HotelMontana'>Hotel de Montaña</option>
+        <option value='HotelEcologico'>Hotel Ecologico</option>
+      </select>
+      <br/>
+      <br/>
       <Input field='amenities' label='Amenities' type="text" value={amenities} handleValueChange={handleChangeAmenities}/>
+      
+      {/*<Input type="file" label="Agregar imagen"   accept="image/png, image/jpg',image/webp,image/jpeg" 
+        handleValueChange={handleChangeImageHotel}/>*/}
       <button>Guardar</button>
     </form>
   </div>
