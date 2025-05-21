72cb3ce4b6ee43d36c7be863f9ebc972c4d5bd31
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-20 20:33:47 -0600

    Se modifica estilo de sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
-
+import './SideBar.css'
 export const SideBar = ({children}) => {
   return (
-    <div style={{ 
+   <div style={{ 
       backgroundColor: '#333333',
-       width: '180px', 
-       padding: '5px', 
-       color:'#ffffff', 
-       height: '100vh', 
-       position: 'fixed',
-       top: 0, 
-       left: 0, 
-       overflowY: 'auto'       
-      }}>
+      width: '180px',
+      padding: '10px',
+      color: '#ffffff',
+      height: '100vh',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      overflowY: 'auto',
+      boxSizing: 'border-box',
+      zIndex: 1000,         
+      }} >
         {children}
         </div>
   )
