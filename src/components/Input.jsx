15cd6894a62197a6186cdf7ff29c9398ce2f34f2
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-20 20:32:11 -0600

    Se realiza cambios en el componente

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export const Input = ({label, value,handleValueChange, type,field, className}) => {
+export const Input = ({label, value,handleValueChange, type,field, className, accept}) => {
     return (
         <>
         <div>
             <label>{label}</label>
         </div>
-        <input className={className} type={type} value={value} onChange={handleValueChange}/>
+        <input className={className} type={type} value={value} onChange={handleValueChange} accept={accept}/>
         </>
     )
 }
