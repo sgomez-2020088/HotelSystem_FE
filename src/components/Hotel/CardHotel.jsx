63b822232617898f899203bc03cdb79e1daa5308
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-20 20:31:24 -0600

    Se realiza cambios en el componente

--- src/components/Hotel/CardHotel.jsx
+++ src/components/Hotel/CardHotel.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 
 import './CardHotel.css'
-export const CardHotel = ({name, address, category,amenities}) => {
+export const CardHotel = ({name, address, category,amenities, image}) => {
+
+  let imageSrc = ''
+   if (image && image.data) {
+    const base64 = btoa(
+      String.fromCharCode(...new Uint8Array(image.data.data || image.data))
+    )
+    imageSrc = `data:${image.contentType};base64,${base64}`
+  }
+
   return (
     <div className="card-hotel">
       <h1 className="hotel-name">Hotel {name}</h1>
       <h2 className="hotel-address">Dirección: {address}</h2>
       <p className="hotel-category">Categoría: {category}</p>
       <p className="hotel-amenities">Amenidades: {amenities}</p>
+     {imageSrc ? (
+        <img src={imageSrc} alt={name} />
+      ) : (
+        <p>No hay imagen</p>
+      )}
     </div>
   )
 }
