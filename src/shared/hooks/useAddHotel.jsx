3e19fc34245920fefbaf63456693d847a1204136
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-21 22:36:17 -0600

    Se modifican hooks

--- src/shared/hooks/useAddHotel.jsx
+++ src/shared/hooks/useAddHotel.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import { addNewHotelRequest } from "../../services/api"
 import toast from "react-hot-toast"
+import { useNavigate } from "react-router-dom"
 
 
 export const useAddHotel = () => {
+    let navigate = useNavigate()
     const addHotel = async(name,address,category,amenities)=>{
         const hotel ={
             name,
@@ -18,6 +20,7 @@ export const useAddHotel = () => {
                 'Error al crear el hotel'
             )
         }
+        navigate('/hotels/Hotels')
         return toast.success('Hotel creado')
     } 
     return {
--- src/shared/hooks/useUpdateHotel.jsx
+++ src/shared/hooks/useUpdateHotel.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { updateHotelRequest } from '../../services/api'
 import toast from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
 
 export const useUpdateHotel = () => {
+    let navigate = useNavigate()
     const updateHotel = async(id,name,address,category,amenities)=>{
         const hotel ={
             id,
@@ -18,6 +20,7 @@ export const useUpdateHotel = () => {
                 'Error al actualizar el hotel'
             )
         }
+        navigate('/hotels/Hotels')
         return toast.success('Hotel actualizado')
     }
   return {
