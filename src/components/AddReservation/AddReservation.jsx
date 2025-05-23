9f3ed93e5ff5e733c9a0253508ef2a2281ac68da
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-23 00:22:41 -0600

    Se modifica componente AddReservation

--- src/components/AddReservation/AddReservation.jsx
+++ src/components/AddReservation/AddReservation.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useAddReservation } from '../../shared/hooks/useAddReservation'
 import { Input } from '../Input'
+import dayjs from 'dayjs'
 
 export const AddReservation = () => {
 
@@ -20,6 +21,9 @@ export const AddReservation = () => {
         services: undefined
     })
 
+    const isDisable = formValidation.checkIn === '' && formValidation.checkOut === '' && formValidation.services===''
+    
+    console.log('Date',dayjs().format('YYYY-MM-DD'))
     const {addReservation}=useAddReservation()
     const handleSubmit = (e)=>{
         e.preventDefault()
@@ -28,18 +32,21 @@ export const AddReservation = () => {
 
     const handleChangeChakeIn = (e)=>{
         const value = e.target.value
+        setFormValidation({...formValidation, checkIn: dayjs(value).isBefore(dayjs(), 'day')?'No puedes reservar en estas fechas':''})
         console.log(value)
         setChekIn(value)
     }
 
     const handleChangeChakeOut = (e)=>{
         const value = e.target.value
+         setFormValidation({...formValidation, checkOut: dayjs(value).isBefore(dayjs(), 'day')?'No puedes reservar en estas fechas': dayjs(value).isBefore(dayjs(checkIn),'day')?'No se puede reservar una fecha antes del checkIn':''})
         console.log(value)
         setCheckOut(value)
     }
 
     const handleChangeServices = (e)=>{
         const value = e.target.value
+        setFormValidation({...formValidation, services: value.length === 0?'No se puede dejar campos vacios':''})
         setServices(value)
     }
   return (
@@ -47,9 +54,12 @@ export const AddReservation = () => {
         <form onSubmit={handleSubmit}>
             <h1> Nueva habitación</h1>
             <Input field='checkIn' type='date'  label='Dia de entrada' value={checkIn} handleValueChange={handleChangeChakeIn}/>
+            <span>{formValidation.checkIn}</span>
             <Input field='checkOut' type='date' label='Dia de salida' value={checkOut} handleValueChange={handleChangeChakeOut}/>
-            <Input field='Services' type='text' label='Servicios extras' value={services} handleValueChange={handleChangeServices}/>
-            <button type='submit'>Guardar</button>
+            <span>{formValidation.checkOut}</span>
+            <Input field='Services' type='text' label='Servicios' value={services} handleValueChange={handleChangeServices}/>
+            <span>{formValidation.services}</span>
+            <button disabled={!isDisable}type='submit'>Guardar</button>
         </form>
     </div>
   )
