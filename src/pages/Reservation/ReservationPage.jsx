125bae7d30e6043a0c59cfdd71da5062367755c7
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-22 20:34:55 -0600

    Se agrega pagina reservation

---
+++ src/pages/Reservation/ReservationPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { ReservationContent } from '../../components/ReservationContent'
+import { NavBar } from '../../components/Navbar/NavBar'
+import { SideBar } from '../../components/SideBar/SideBar'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+
+export const ReservationPage = () => {
+
+  let {idhotel} = useParams()
+
+  let navigate = useNavigate(idhotel)
+  return (
+    <>  
+        <NavBar className="navbar2"> 
+            <h1>Reservation</h1>
+        </NavBar>
+
+        <div className='container2-plus'>
+            <SideBar>
+                 <ul>
+                      <li onClick={()=>{navigate(`/reservation/myReservations/${idhotel}`)}}><Link className="sidebar-link">Ver mis reservaciones</Link></li>   
+                     <li onClick={()=>{navigate(`/rooms/rooms/${idhotel}`)}}><Link className="sidebar-link">Ver habitaciones</Link></li>   
+                </ul>
+            </SideBar>
+        </div>
+        <ReservationContent/>
+    </>
+  )
+}
